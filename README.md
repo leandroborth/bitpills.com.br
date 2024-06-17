931a12ae0d011eef3689b6fc04fbc471a62846b3
leandroborth <leandroborth@gmail.com>
2024-06-17 15:45:25 -0300

    Atualizados `README` e  página inicial (em construção)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # [bitpills.com.br](http://bitpills.com.br/)
 
-O [BitPills](http://bitpills.com.br/) é o ponto de encontro para interessados em Bitcoin, Economia, Política e Tecnologia, destinado a debates e troca de conhecimentos por meio de publicações e comentários criados pelos próprios usuários.
+O [BitPills](http://bitpills.com.br/) é o ponto de encontro para interessados em Bitcoin, Economia e Tecnologia, destinado a debates e troca de conhecimentos por meio de publicações e comentários criados pelos próprios usuários.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,45 @@
 function Home() {
-  return <h1>Index works!</h1>;
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "95vh",
+        fontFamily: "Helvetica, sans-serif",
+        paddingLeft: "25px",
+        paddingRight: "25px",
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center",
+          fontSize: "24px",
+          fontWeight: "bold",
+          color: "#333",
+        }}
+      >
+        BitPills
+      </h1>
+      <p
+        style={{
+          textAlign: "center",
+          fontSize: "18px",
+          color: "#555",
+        }}
+      >
+        Estamos construindo um novo local na internet!
+        <br />
+        <br />
+        Um ponto de encontro pra quem se interessa por Bitcoin, Economia e
+        Tecnologia.
+        <br />
+        Um espaço destinado a debates e troca de conhecimentos por meio de
+        publicações e comentários criados pelos próprios usuários.
+      </p>
+    </main>
+  );
 }
 
 export default Home;
